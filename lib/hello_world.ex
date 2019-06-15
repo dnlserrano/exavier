26f7eb7262f7d848c77bccbdb9ad752c89874bfa
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2019-06-15 22:19:34 +0100

    Simpler sample file for testing
    Simpler, but with a twist. Two uses of the == operator. Two
    opportunities for mutation, you get me?

--- lib/hello_world.ex
+++ lib/hello_world.ex
@@ -1,15 +1,6 @@
 defmodule HelloWorld do
-  def zero?(val), do: val == 0
-
-  def divide(a, b) do
-    if b == 0 do
-      0
-    else
-      a / b
-    end
-  end
-
-  def sum_square(a, b) do
-    a * a + b * b
+  def zero?(val) do
+    IO.inspect(val == 3)
+    val == 0
   end
 end
