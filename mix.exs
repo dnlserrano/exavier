7243a3b3018b8c4b59d37546eae37210fc1b3c7c
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2019-07-24 21:47:46 +0100

    Update mix.exs
    Prepare for release, lol such fail

--- mix.exs
+++ mix.exs
@@ -7,22 +7,32 @@ defmodule Exavier.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp description do
+    "Elixir mutation testing library"
+  end
+
+  defp deps, do: []
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      maintainers: ["Daniel Serrano"],
+      licenses: ["MIT"],
+      links: %{
+        github: "https://github.com/dnlserrano/exavier",
+        personal: "https://dnlserrano.dev",
+      },
     ]
   end
 end
