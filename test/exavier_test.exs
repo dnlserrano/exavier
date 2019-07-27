b8b943ee41fcf78304d1346d0c7e7d1fdb688d45
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2019-07-27 19:23:52 +0100

    Add tests for Exavier.timeout/1

--- test/exavier_test.exs
+++ test/exavier_test.exs
@@ -17,4 +17,34 @@ defmodule ExavierTest do
   test "translate test file to module" do
     assert @subject.test_file_to_module("test/exavier_test.exs") == Exavier
   end
+
+  test "when EXAVIER_DEBUG=1 timeout defaults to infinity" do
+    System.put_env("EXAVIER_DEBUG", "1")
+    on_exit(self(), fn -> System.delete_env("EXAVIER_DEBUG") end)
+
+    assert @subject.timeout(:anything) == :infinity
+  end
+
+  test "when EXAVIER_DEBUG=true timeout defaults to infinity" do
+    System.put_env("EXAVIER_DEBUG", "true")
+    on_exit(self(), fn -> System.delete_env("EXAVIER_DEBUG") end)
+
+    assert @subject.timeout(:anything) == :infinity
+  end
+
+  test "when getting timeout for running whole mutation testing" do
+    assert @subject.timeout(:mutate_everything) == 60_000
+  end
+
+  test "when getting timeout for when mutating a module" do
+    assert @subject.timeout(:mutate_module) == 5000
+  end
+
+  test "when getting timeout for when reporting" do
+    assert @subject.timeout(:report) == 1000
+  end
+
+  test "when getting timeout for unknown action uses default timeout" do
+    assert @subject.timeout(:unknown) == 5000
+  end
 end
