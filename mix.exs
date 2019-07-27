2a638abcc851e2ea88a41c256d6da5f06f23347d
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2019-07-27 10:48:47 +0100

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exavier.MixProject do
   def project do
     [
       app: :exavier,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
