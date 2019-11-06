2cd1817190dd9e7cf6ac48ae9b63d7192f61f690
KingOfRostov <mk-serg@yandex.ru>
2019-11-06 18:07:39 +0300

    Handle function references properly

--- lib/foo_bar.ex
+++ lib/foo_bar.ex
@@ -1,3 +1,7 @@
 defmodule FooBar do
   def sub(a, b), do: a - b
+
+  def list_sum(list), do: Enum.reduce(list, 0, &add/2)
+
+  defp add(a, b), do: a + b
 end
--- test/foo_bar_test.exs
+++ test/foo_bar_test.exs
@@ -3,7 +3,11 @@ defmodule FooBarTest do
 
   @subject FooBar
 
-  test "when 0, 0" do
+  test "when 0, sub 0" do
     assert @subject.sub(0, 0) == 0
+  end
+
+  test "when [1, 2, 3], list_sum 6" do
+    assert @subject.list_sum([1, 2, 3]) == 6
   end
 end
