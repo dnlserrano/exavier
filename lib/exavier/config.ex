d1f61050908a835e65d1417da96fdd68aa19c279
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2019-12-29 13:11:39 +0000

    Bulletproof load .exavier.exs config file
    If present, we validate that the file is OK. If not, we error out before
    even starting an exavier mutation test run.

--- lib/exavier/config.ex
+++ lib/exavier/config.ex
@@ -3,22 +3,40 @@ defmodule Exavier.Config do
   @config_file_content File.read!("priv/" <> @config_file)
 
   def get(key, default \\ nil) do
-    file = config_file()
+    case exists?() do
+      true ->
+        load_configs()
+        |> do_get(key, default)
 
-    unless exists?() do
-      raise("Config file #{file} not found")
+      _ ->
+        default
     end
-
-    {configs, _binding} =
-      file
-      |> File.read!()
-      |> Code.eval_string()
-
-    do_get(configs, key, default)
   end
 
   defp do_get(nil, _key, default), do: default
   defp do_get(configs, key, default), do: Map.get(configs, key, default)
+
+  def ensure_loaded_configs do
+    if exists?() do
+      load_configs()
+    end
+  rescue
+    _ ->
+      raise("""
+      Error reading configuration file \"#{config_file()}\".
+      Make sure the content of your configuration file is a valid Elixir map.
+      Alternatively, generate a sample configuration file via the mix task \"mix exavier.config\".
+      """)
+  end
+
+  defp load_configs do
+    {configs, _binding} =
+      config_file()
+      |> File.read!()
+      |> Code.eval_string()
+
+    configs
+  end
 
   def exists?, do: config_file() |> File.exists?()
 
