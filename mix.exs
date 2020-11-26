4fb6796597f487e901cda1ac0b726d501a28e591
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2020-11-26 00:21:20 +0000

    Bump 0.1.5 -> 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exavier.MixProject do
   def project do
     [
       app: :exavier,
-      version: "0.1.5",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
