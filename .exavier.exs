dabb37bbf544652df75d445b77a4b387c962c447
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-11-27 11:08:10 -0700

    Configure Exavier run for new mutators

--- .exavier.exs
+++ .exavier.exs
@@ -3,7 +3,11 @@
   test_files_to_modules: %{
     "test/exavier/mutators/aor1_test.exs" => Exavier.Mutators.AOR1,
     "test/exavier/mutators/aor2_test.exs" => Exavier.Mutators.AOR2,
+    "test/exavier/mutators/aor3_test.exs" => Exavier.Mutators.AOR3,
+    "test/exavier/mutators/aor4_test.exs" => Exavier.Mutators.AOR4,
     "test/exavier/mutators/ror1_test.exs" => Exavier.Mutators.ROR1,
+    "test/exavier/mutators/ror2_test.exs" => Exavier.Mutators.ROR2,
+    "test/exavier/mutators/ror3_test.exs" => Exavier.Mutators.ROR3,
     "test/exavier/mutators/ror4_test.exs" => Exavier.Mutators.ROR4
   }
 }
