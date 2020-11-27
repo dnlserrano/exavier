7a22c0910c4432a2bc1c15c26edd9dc1cba4cb8f
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-11-27 11:16:27 -0700

    Fix ROR1 test (fixes #16)

--- test/exavier/mutators/ror1_test.exs
+++ test/exavier/mutators/ror1_test.exs
@@ -1,44 +1,50 @@
 defmodule Exavier.Mutators.ROR1Test do
   use ExUnit.Case, async: true
 
-  @subject Exavier.Mutators.AOR1
+  @subject Exavier.Mutators.ROR1
 
   @mutations [
     %{
-      description: "mutates + to -",
-      original_code: {:+, [line: 2], [1, 2]},
+      description: "mutates < to <=",
+      original_code: {:<, [line: 2], [1, 2]},
       lines_to_mutate: [2],
-      mutated_code: {:-, [line: 2], [1, 2]}
+      mutated_code: {:<=, [line: 2], [1, 2]}
     },
     %{
-      description: "mutates - to +",
-      original_code: {:-, [line: 2], [1, 2]},
+      description: "mutates <= to <",
+      original_code: {:<=, [line: 2], [1, 2]},
       lines_to_mutate: [2],
-      mutated_code: {:+, [line: 2], [1, 2]}
+      mutated_code: {:<, [line: 2], [1, 2]}
     },
     %{
-      description: "mutates * to /",
-      original_code: {:*, [line: 2], [1, 2]},
+      description: "mutates > to <",
+      original_code: {:>, [line: 2], [1, 2]},
       lines_to_mutate: [2],
-      mutated_code: {:/, [line: 2], [1, 2]}
+      mutated_code: {:<, [line: 2], [1, 2]}
     },
     %{
-      description: "mutates / to *",
-      original_code: {:/, [line: 2], [1, 2]},
+      description: "mutates >= to <",
+      original_code: {:>=, [line: 2], [1, 2]},
       lines_to_mutate: [2],
-      mutated_code: {:*, [line: 2], [1, 2]}
+      mutated_code: {:<, [line: 2], [1, 2]}
     },
     %{
-      description: "mutates rem to *",
-      original_code: {:rem, [line: 2], [1, 2]},
+      description: "mutates == to <",
+      original_code: {:==, [line: 2], [1, 2]},
       lines_to_mutate: [2],
-      mutated_code: {:*, [line: 2], [1, 2]}
+      mutated_code: {:<, [line: 2], [1, 2]}
+    },
+    %{
+      description: "mutates != to <",
+      original_code: {:!=, [line: 2], [1, 2]},
+      lines_to_mutate: [2],
+      mutated_code: {:<, [line: 2], [1, 2]}
     },
     %{
       description: "mutates recursively if operator is repeated",
-      original_code: {:rem, [line: 2], [{:rem, [line: 3], [2, 3]}, 2]},
+      original_code: {:==, [line: 2], [{:<, [line: 3], [2, 3]}, false]},
       lines_to_mutate: [2, 3],
-      mutated_code: {:*, [line: 2], [{:*, [line: 3], [2, 3]}, 2]}
+      mutated_code: {:<, [line: 2], [{:<=, [line: 3], [2, 3]}, false]}
     }
   ]
 
